d6d12a0673904437f2c9ce88646f5806bdbd880e
atrox3d <atrox3d@gmail.com>
2021-02-18 18:27:09 +0100

    , sticky="EW"

--- main.py
+++ main.py
@@ -24,18 +24,24 @@ password_label = tkinter.Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # entries
-website_entry = tkinter.Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=3)
-email_entry = tkinter.Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-password_entry = tkinter.Entry(width=21)
-password_entry.grid(row=3, column=1)
+website_entry = tkinter.Entry(
+    # width=35
+)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+email_entry = tkinter.Entry(
+    # width=35
+)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+password_entry = tkinter.Entry(
+    # width=21
+)
+password_entry.grid(row=3, column=1, sticky="EW")
 
 # buttons
 generate_password_button = tkinter.Button(text="Generate Password")
-generate_password_button.grid(row=3, column=2)
+generate_password_button.grid(row=3, column=2, sticky="EW")
 add_button = tkinter.Button(text="add", width=36)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 
 ########################################################################
