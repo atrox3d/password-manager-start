39bbfb5011673e15901c13bf8a9b7ef578025b2c
atrox3d <atrox3d@gmail.com>
2021-02-19 05:16:24 +0100

    add messageboxes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 import mypass
 
 
@@ -10,10 +11,19 @@ def save():
     account = account_entry.get()
     password = password_entry.get()
 
-    mypass.save(website, account, password)
-
-    website_entry.delete(0, tkinter.END)
-    password_entry.delete(0, tkinter.END)
+    confirm = tkinter.messagebox.askokcancel(title=website, message=f"details entered:\n"
+                                                                    f"account: {account}\n"
+                                                                    f"password: {password}\n"
+                                                                    "Confirm save?"
+                                             )
+    if confirm:
+        # at the moment mypass.save() returns True
+        if mypass.save(website, account, password):
+            tkinter.messagebox.showinfo(title=website, message="account saved correctly")
+            website_entry.delete(0, tkinter.END)
+            password_entry.delete(0, tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title=website, message="account not saved")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
--- mypass.py
+++ mypass.py
@@ -6,5 +6,5 @@ def save(website, email, password):
     with open(PASSWORD_FILE, APPEND) as file:
         print(f"saving: {website} | {email} | {password} on {PASSWORD_FILE}")
         file.write(f"{website} | {email} | {password}\n")
-
+    return True
 
